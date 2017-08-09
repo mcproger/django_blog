23d0af320c637143a305b42f5d9c66186179680e
mcproger <artem7lomonosov@gmail.com>
2017-08-09 15:14:41 +0300

    Add user authenticate

---
+++ login/views.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from django.shortcuts import render, redirect
+from django.contrib import auth 
+
+
+def login(request):
+	if request.POST:
+		username = request.POST.get('username', None)
+		password = request.POST.get('password', None)
+		user = auth.authenticate(username=username, password=password)
+		if user is not None:
+			auth.login(request, user)
+			return redirect('/')
+		else:
+			return render(request, 'login.html')
+	else:
+		return render(request, 'login.html')
+
+
+def logout(request):
+	auth.logout(request)
+	return redirect('/')
