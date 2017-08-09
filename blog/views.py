0b11073daba9f0b1ee2f46e58ea08a2444323c9d
mcproger <artem7lomonosov@gmail.com>
2017-08-09 15:15:04 +0300

    Some fixes

--- blog/views.py
+++ blog/views.py
@@ -2,15 +2,18 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+from django.contrib import auth
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    username = auth.get_user(request).username
+    return render(request, 'blog/post_list.html', {'posts': posts, 'username': username})
 
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    username = auth.get_user(request).username
+    return render(request, 'blog/post_detail.html', {'post': post, 'username': username})
 
 
 def post_new(request):
