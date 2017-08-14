e7f1f4441e1391f873f4b2774c60cd22ab1a5102
mcproger <artem7lomonosov@gmail.com>
2017-08-14 16:16:56 +0300

    Fix logout

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
 	url(r'^admin/', include(admin.site.urls)),
-	url(r'^auth/', include('login.urls')),
 	url(r'', include('blog.urls')),
+	url(r'auth/', include('login.urls')),
+	
 ]
