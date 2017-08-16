1a9b1016fe4f50ae86fbbbb162ef74417e9484fd
mcproger <artem7lomonosov@gmail.com>
2017-08-16 21:22:50 +0300

    Fix problem with urls

--- login/urls.py
+++ login/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^login/', views.login),
-    url(r'^logout/', views.logout),
-    url(r'^register/', views.register),
+    url(r'login/$', views.login, name='login'),
+    url(r'logout/$', views.logout, name='logout'),
+    url(r'register/$', views.register, name='register'),
 ]  
