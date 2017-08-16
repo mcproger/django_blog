2df5dbedb23c90f19eee3c2174db22a4acb43a55
mcproger <artem7lomonosov@gmail.com>
2017-08-16 18:55:50 +0300

    Some fixes

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 urlpatterns = [
 	url(r'^admin/', include(admin.site.urls)),
 	url(r'', include('blog.urls')),
-	url(r'auth/', include('login.urls')),
+	url(r'^auth/', include('login.urls')),
 	
 ]
