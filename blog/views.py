23373ab6d872f630925bf482412ac8adba63fd16
mcproger <artem7lomonosov@gmail.com>
2017-08-17 14:18:19 +0300

    Fix ordering of posts

--- blog/views.py
+++ blog/views.py
@@ -4,16 +4,16 @@ from .models import Post
 from .forms import PostForm
 from django.contrib import auth
 
+
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    username = auth.get_user(request).username
-    return render(request, 'blog/post_list.html', {'posts': posts, 'username': username})
+    posts = Post.objects.filter(
+        published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    username = auth.get_user(request).username
-    return render(request, 'blog/post_detail.html', {'post': post, 'username': username})
+    return render(request, 'blog/post_detail.html', {'post': post})
 
 
 def post_new(request):
@@ -22,7 +22,6 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            #post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -37,7 +36,6 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            #post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -46,7 +44,8 @@ def post_edit(request, pk):
 
 
 def post_draft_list(request):
-    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    posts = Post.objects.filter(
+        published_date__isnull=True).order_by('created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
 
