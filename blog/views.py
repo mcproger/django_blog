f4fb9a8431068e5f7ed3743be7384114438d9f99
mcproger <artem7lomonosov@gmail.com>
2017-08-20 15:08:05 +0300

    Remove usless import

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
-from django.contrib import auth
 
 
 def post_list(request):
