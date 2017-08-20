7d527d87ab5966d88978a7efd33a39f23153f8b3
mcproger <artem7lomonosov@gmail.com>
2017-08-20 22:09:21 +0300

    Add test_post_list

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,18 @@
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 
-# Create your tests here.
+from . import models
+
+
+class BlogViewsTest(TestCase):
+    def setUp(self):
+        pass
+
+    def get_queryset(self):
+        return models.Post.objects.all()
+
+    def test_post_list(self):
+        response = self.client.get(reverse('post_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(
+            response.context['posts'], self.get_queryset())
